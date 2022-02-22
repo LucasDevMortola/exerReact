3cebdea3a492413a7872fc6276699ae372c70bfe
Lucas Mortola da cunha <lucas@allto.digital>
2022-02-22 12:59:13 -0300

    primeiro commit

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import {HeaderPage} from './style.js'
+import{Titulo} from './style.js'
+
+export default function Header (){
+    return(
+        <HeaderPage>
+            <Titulo>Meu primeiro aplicativo em React</Titulo> 
+        </HeaderPage>
+    )
+}
---
+++ src/components/header/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import img from "../../img/imgmar.jpg"
+
+export const HeaderPage = styled.div`
+background-image: url(${img});
+text-align: center;
+hight: 200px;
+padding: 100px 0px
+`
+export const Titulo = styled.h1`
+color: #fefefe;    
+font-size: 3em;
+`
